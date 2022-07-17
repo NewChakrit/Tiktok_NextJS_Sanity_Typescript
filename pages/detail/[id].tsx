59ee9c4959fdbd0a7d52b26190cdf1da6156e42f
chakrit tnasinpaibool <newchakrit@Administrators-MacBook-Pro-7.local>
2022-07-17 16:10:16 +0700

    axios config

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import axios from 'axios';
 
-const Detail = () => {
+import { GoVerified } from 'react-icons/go';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BASE_URL } from '../../utils';
+import { Video } from '../../types';
+
+interface IProps {
+	postDetails: Video;
+}
+
+const Detail = ({ postDetails }: IProps) => {
+	const [post, setPost] = useState(postDetails);
+
+	const videoRef = useRef(null);
+
+	if (!post) return null;
+
 	return <div>Details</div>;
+};
+
+export const getServerSideProps = async ({
+	params: { id },
+}: {
+	params: { id: string };
+}) => {
+	const { data } = await axios.get(`${BASE_URL}/api/post/${id}`);
+
+	return {
+		props: {
+			postDetails: data,
+		},
+	};
 };
 
 export default Detail;
