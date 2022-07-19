93fc93abe2435ba21646ab23f13d09021196f839
chakrit tnasinpaibool <imnew108@gmail.com>
2022-07-19 22:28:44 +0700

    fetch alluser

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -59,7 +59,7 @@ const Detail = ({ postDetails }: IProps) => {
 		}
 	};
 
-	const addComment = async (e) => {
+	const addComment = async (e: any) => {
 		e.preventDefault();
 
 		if (userProfile && comment) {
