4a6f24d2935f89bbbf07486538b9359abcca4ce2
chakrit tnasinpaibool <imnew108@gmail.com>
2022-07-26 22:00:59 +0700

    save

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -24,7 +24,6 @@ const Profile = ({ data }: IProps) => {
 
 	const videos = showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
 	const liked = !showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
-	// console.log(videosList);
 
 	useEffect(() => {
 		if (showUserVideos) {
